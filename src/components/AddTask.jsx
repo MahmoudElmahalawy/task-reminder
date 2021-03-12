14321560a3d2c772569180e4fc62d775ab71804f
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-03-12 14:53:05 +0200

    Updated README file and error message

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,18 +1,21 @@
 import {useState} from 'react'
+import {motion} from 'framer-motion'
 
 
 const AddTask = ({onAdd}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
+    const [errorEmpty, setErrorEmpty] = useState(false)
 
     const onSubmit = e => {
         e.preventDefault()
 
         if(!text || !day){
-            alert('من فضلك املأ الخانات أولاً')
-            return
+            //alert('من فضلك املأ الخانات أولاً')
+            return setErrorEmpty(true)
         }
+        setErrorEmpty(false)
         onAdd({text, day, reminder})
         setText('')
         setDay('')
@@ -21,6 +24,7 @@ const AddTask = ({onAdd}) => {
 
     return (
         <form className="add-from" onSubmit={onSubmit}>
+            {errorEmpty ? <motion.div animate={{ opacity: [0, 1, 0, 1] }} transition={{ duration: 0.5 }} className="form-error-empty">برجاء ملأ خانتي المهمة والوقت</motion.div> : ''}
             <div className="form-control">
                 <label>مهمة جديدة</label>
                 <input type="text" placeholder="إضافة مهمة" value={text} onChange={(e) => setText(e.target.value)}/>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,13 +5,13 @@ const Task = ({task, onToggle, onDelete}) => {
     return (
         <motion.div
         //  animate={{ scale: [1, 1.125, 1] }}
-         whileHover={{ scale: 1.002, border: '2px solid black' }}
+         whileHover={{ scale: 1.002 }}
          whileTap={{ scale: 0.99 }}
          transition={{ duration: 0.2 }}
          className='task' 
          onDoubleClick={() => onToggle(task.id)} >
             {task.reminder ? <FaRegClock className='reminder' onClick={() => onToggle(task.id)} /> : ''}
-            <h3>{task.text} <FaTimes className='delete-icon' style={{marginTop: '1.5rem'}} onClick={() => onDelete(task.id)} /></h3>
+            <h3>{task.text} <FaTimes className='delete-icon' onClick={() => onDelete(task.id)} /></h3>
             <p>{task.day.replace("T", " | الساعة  ")}</p>            
         </motion.div>
     )
